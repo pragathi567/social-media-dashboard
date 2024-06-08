72c566890dc2f59bd2fd2ded5fea0e017152937a
pragathi567 <ravadapragathi.ucen@gmail.com>
2024-06-08 11:19:11 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./components/Header"
+import Followers from "./components/Followers"
+import Overview from "./components/Overview"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className="p-8 dark:bg-slate-900 h-full lg:h-screen">
+        <div
+          className="bg-slate-100 dark:bg-slate-800 h-52 w-full absolute top-0 left-0 rounded-b-3xl"
+          style={{
+            zIndex: 0,
+          }}
+        ></div>
+        <Header />
+        <Followers />
+        <Overview />
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
